0a78c3eb773b2bb3aec72c61d2cac913f2f98056
Marine Allavoine <allavoine.marine@gmail.com>
2023-12-28 01:20:48 +0100

    Corrections Login Création modale (WIP)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -18,46 +18,114 @@
 //si on a un token
 //on le stocke dans une variable
 //on le stocke dans le local storage
-//on stocke le fait que l'utilisateur est connecté
 //on redirige vers la page d'accueil
 //on affiche un message
 //sinon
 //on affiche un message
 //fin de la condition
 
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector('form');
-  
-   form.addEventListener("submit", async function (e) {
-      e.preventDefault();
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      const user = {
-        email: email,
-        password: password,
-      };
-  
-      // Récupération des valeurs de l'API
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: 'POST',
-        headers: { 
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-      });
-  
-    if (response.ok) {
-            const data = await response.json();
-            if (data.token) {
-                const token = data.token;
-                localStorage.setItem('token', token);
-                localStorage.setItem('isLoggedIn', true);
-                window.location.href = "index.html";//revoir pour une fois connectée le retour
-                alert("Vous êtes bien connecté");
-  
+// où est stocké le token ?
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+const user = { email, password};
+const logOutBtn = document.getElementById("logOut");
+const form = document.querySelector('form');
+
+
+
+function main(){
+     logIn() ;
+        logOut();
+        
+
+}
+
+async function logIn() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const email = document.querySelector('#email').value;
+            const password = document.querySelector('#password').value;
+            const user = {
+                email,
+                password
             };
-    }else{
-        alert("Nope");
-    };
-  });
-});
+console.log(user);
+try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+           method : 'POST',
+             headers : {
+                'Content-Type': 'application/json'
+            },
+             body : JSON.stringify(user)
+          });
+console.log(response);
+
+
+if (response.status === 200) {
+    //connection ok on recup le token
+    const data = await response.json();//recupere la reponse en json
+    let idtoken = data.token;//recupere le token
+    window.localStorage.setItem("token",idtoken) //stock token dans localStorage
+    window.location.href="index.html" //Retour sur la page d'accueil
+ 
+         
+           // if (response.ok) {
+             //   const data = await response.json();
+             //  if (data.token) {
+                 //   const token = data.token;
+                    //const token = window.localStorage.token; // ??
+               //     window.localStorage.setItem('token', data.token);
+                 //   window.localStorage.logedIn = true;
+                   // window.location.href = './index.html';
+                    alert("Vous êtes bien connecté");
+                
+
+
+            } else {
+               
+                alert("nope");
+            }
+
+            console.log(response);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    )})}
+logIn();
+
+
+// travailler sur le LogOut
+
+
+function logOut() {//fonction pour se deconnecter
+    const logOutBtn = document.getElementById("logOut");//recupere le bouton
+
+  
+    if (window.localStorage.getItem("token")) {//si le token est dans le local storage
+      logOutBtn.innerHTML = "logout";//changer le texte du bouton
+  
+      logOutBtn.addEventListener("click", () => {//ajouter un event listener sur le bouton
+        logOutBtn.href = window.location.href;//rediriger vers la page d'accueil
+        window.localStorage.removeItem("token");//supprimer le token du local storage
+      });
+    }
+  }
+  
+  logOut();
+
+
+//
+  //function logout() {
+    //let login = document.getElementById("logOut");
+    //login.innerHTML = "logout";
+    //login.setAttribute("href", "#");
+    //login.addEventListener("click", function logout() {
+     // window.localStorage.removeItem("token");
+      //window.location.href = "./index.html";
+    //});
+  //}
+  //logout  ();
